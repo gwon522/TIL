d559ec9d3944b11a4a216cf068cc479361cfa0db
gwon <53310111+gwon522@users.noreply.github.com>
2022-03-11 23:07:45 +0900

    d

--- Algorithm/src/Run.java
+++ Algorithm/src/Run.java
@@ -1,9 +1,16 @@
-import Programmers.ParenthesisChange;
+import Algo.AntSolider;
+import Algo.MinMoney;
+import Algo.Test;
+import BeakJoon.To1;
+import LeetCode.*;
 
 public class Run {
     public static void main(String[] args) {
-        String p = "()))((()";
         
-       System.out.println(ParenthesisChange.answer(p));
+        // MinMoney mm = new MinMoney();
+        // System.out.println(mm.minMoney(2, 15, new int[]{2,3}));
+        Test a = new Test();
+
+        a.ans(3, new int[][]{{5,7,6,6},{3,9,5,4},{8,2,7,6}});
     }
 }
